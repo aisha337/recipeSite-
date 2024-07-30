c6a1206e3a8a73c29ab1ccc9e48861399e7911ab
Aisha Ali <aishaali@TKC-0249.local>
2024-07-30 10:44:05 +0100

    created mains and routing added

--- recipe-site/src/App.js
+++ recipe-site/src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Routes,Link} from 'react-router-dom'
+import Mains from './pages/Mains';
 
 function App() {
   return (
+    <BrowserRouter> 
+    <>
     <div className="App">
       <h1> Recipe Site</h1>
     </div>
-  );
+    <nav className="navbar">
+    
+      {/* <a className="navbar-link" href="https://www.google.com/" target="_blank"rel="noreferrer">Home</a> */}
+      {/* <a className="navbar-link" href="https://www.google.com/">Mains</a> */}
+      <Link to='/mains'>Mains </Link>
+      {/* <a className="navbar-link" href="https://www.google.com/">Ingredients</a>
+      <a className="navbar-link" href="https://www.google.com/">Desserts</a> */}
+    
+   </nav>
+   <Routes>
+    <Route path='/mains' element={<Mains/>}></Route>
+    
+   </Routes>
+
+   </>
+   </BrowserRouter>
+   );
+  
 }
+  
 
 export default App;
---
+++ recipe-site/src/pages/Mains.js
@@ -0,0 +1,7 @@
+const Mains = () => {
+    return (
+        <h1> Mains</h1>
+    )
+}
+
+export default Mains
